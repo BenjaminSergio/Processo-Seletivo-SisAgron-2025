36bd26c759a7dfd85b401d341d2cb8d1112d1170
Benjamin Sergio <bssergio2@gmail.com>
2025-02-20 08:50:37 -0300

    Firebase remove API key

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBMyByfTQNWxTtDuPUD4d0rzGiSubsHhZo",
-    authDomain: "processo-seletivo-sisagron.firebaseapp.com",
-    projectId: "processo-seletivo-sisagron",
-    storageBucket: "processo-seletivo-sisagron.firebasestorage.app",
-    messagingSenderId: "52977134307",
-    appId: "1:52977134307:web:90f5ff535b71d68d6433d6",
-    measurementId: "G-38K0Z7B5CH"
-  };
+  apiKey: 'SUA_API_KEY',
+  authDomain: 'SEU_AUTH_DOMAIN',
+  projectId: 'SEU_PROJECT_ID',
+  storageBucket: 'SEU_STORAGE_BUCKET',
+  messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
+  appId: 'SEU_APP_ID'
+};
+
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
